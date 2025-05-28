c94c0c7e1595889f6d2923912ab469ba51a977c7
Bhanuka <bhanuka0428@gmail.com>
2025-05-28 12:34:08 +0530

    Initial commit

---
+++ src/api/checkLogin.js
@@ -0,0 +1,20 @@
+const checkLogin = async (token) => (
+   
+
+    fetch(token[0]+'wp-json/meup/v1/check_login/',
+    {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            
+        },
+        body: JSON.stringify({ 
+            token: token[1],
+        })
+    })
+    .then( res => res.json() )
+);
+
+
+module.exports = checkLogin;
