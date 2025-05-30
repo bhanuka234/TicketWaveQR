d3d813698eea73299bf1aae1bdfa723ff37ca878
watagalawgbn <watagalawgbn.21@uom.lk>
2025-05-30 12:28:03 +0530

    loading screen is done

--- src/screens/AuthLoadingScreen.js
+++ src/screens/AuthLoadingScreen.js
@@ -1,13 +1,17 @@
-//src/components/AuthLoadingScreen.js
-
-import React, {Component} from 'react';
-import {ActivityIndicator, StatusBar, StyleSheet, View} from 'react-native';
+import React, { Component } from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  Image,
+  ActivityIndicator,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 import PropTypes from 'prop-types';
-
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-
 import getToken from '../api/getToken';
 import checkLogin from '../api/checkLogin';
+import GradientBackground from '../components/GradientBackground'; // Assuming this provides the background gradient
 
 export default class AuthLoadingScreen extends Component {
   constructor(props) {
@@ -17,10 +21,22 @@ export default class AuthLoadingScreen extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <ActivityIndicator />
-        <StatusBar barStyle="default" />
-      </View>
+      <>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <GradientBackground>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={styles.container}>
+            <Image source={require('../assets/logo.png')} style={styles.logo} />
+            <ActivityIndicator size="70" color="#ffffff" style={styles.spinner} />
+            <Text style={styles.loadingText}>Loading...</Text>
+          </KeyboardAvoidingView>
+        </GradientBackground>
+      </>
     );
   }
 
@@ -29,7 +45,7 @@ export default class AuthLoadingScreen extends Component {
       .then(token => checkLogin(token))
       .then(res => {
         this.props.navigation.navigate(
-          res.status === 'SUCCESS' ? 'Login' : 'Login',
+          res.status === 'SUCCESS' ? 'Login' : 'Login'
         );
       })
       .catch(() => this.props.navigation.navigate('Login'));
@@ -47,7 +63,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 5,
-    backgroundColor: '#c0d6f1',
+  },
+  logo: {
+    width: 250,
+    height: 250,
+    resizeMode: 'contain',
+    marginBottom: 30,
+  },
+  spinner: {
+    marginBottom: 10,
+  },
+  loadingText: {
+    color: '#ffffff',
+    fontSize: 20,
   },
 });
