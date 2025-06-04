11306cd40ab0c4dcf2acc1d5fc5d01a68029a7d1
Janani Lasindu <jananilasindu@gmail.com>
2025-06-04 12:48:08 +0530

    status bar

--- src/screens/Events.js
+++ src/screens/Events.js
@@ -6,6 +6,8 @@ import {
   TouchableOpacity,
   FlatList,
   StatusBar,
+  Image,
+  SafeAreaView
 } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import getToken from '../api/getToken';
@@ -33,56 +35,94 @@ class Events extends Component {
     this.props.navigation.navigate('Login');
   }
 
+  // Add a function to handle going back
+  handleBack = () => {
+    this.props.navigation.goBack();
+  };
+
   render() {
     return (
-      <GradientBackground>
+      <>
         <StatusBar
           translucent
           backgroundColor="transparent"
           barStyle="light-content"
         />
-        <View style={styles.container}>
-          
-
-          <FlatList
-            data={this.state.data}
-            renderItem={({ item, index }) => (
-              <View style={styles.card}>
-                <Text style={styles.indexText}>{index + 1}.)</Text>
-                <Text style={styles.titleText}>{item.post_title}</Text>
-                <TouchableOpacity
-                  style={styles.viewButton}
-                  onPress={() =>
-                    this.props.navigation.navigate('ListTickets', {
-                      eid: parseInt(item.ID),
-                      title: item.post_title,
-                    })
-                  }
-                >
-                  <Text style={styles.viewText}>View</Text>
-                </TouchableOpacity>
-              </View>
-            )}
-            keyExtractor={item => item.post_title}
-          />
-        </View>
-      </GradientBackground>
+        <GradientBackground>
+          <SafeAreaView style={styles.safe}>
+            <View style={styles.header}>
+              <TouchableOpacity
+                onPress={this.handleBack}
+                style={styles.backBtn}>
+                <View style={styles.backContent}>
+                  <Image
+                    source={require('../assets/back.png')}
+                    style={styles.backIcon}
+                    resizeMode="contain"
+                  />
+                  <Text style={styles.backText}>Events</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.container}>
+              <FlatList
+                data={this.state.data}
+                renderItem={({ item, index }) => (
+                  <View style={styles.card}>
+                    <Text style={styles.indexText}>{index + 1}.)</Text>
+                    <Text style={styles.titleText}>{item.post_title}</Text>
+                    <TouchableOpacity
+                      style={styles.viewButton}
+                      onPress={() =>
+                        this.props.navigation.navigate('ListTickets', {
+                          eid: parseInt(item.ID),
+                          title: item.post_title,
+                        })
+                      }
+                    >
+                      <Text style={styles.viewText}>View</Text>
+                    </TouchableOpacity>
+                  </View>
+                )}
+                keyExtractor={item => item.post_title}
+              />
+            </View>
+          </SafeAreaView>
+        </GradientBackground>
+      </>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safe: {
     flex: 1,
-    padding: 2,
-    paddingTop: 20,
+    paddingHorizontal: 20,
   },
-  heading: {
-    fontSize: 24,
-    color: '#ff66cc',
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: -50,
+  },
+
+  title: {
+    fontSize: 18,
     fontWeight: 'bold',
-    marginBottom: 20,
-    alignSelf: 'center',
+    color: '#fff',
+    textAlign: 'left',
+    marginTop: 10,
+    paddingHorizontal: 10,
+  },
+  backContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  backText: {
+    fontSize: 25,
+    color: '#FF71D2',
+    marginLeft: -30,
+    fontWeight: '500',
   },
   card: {
     backgroundColor: '#222',
